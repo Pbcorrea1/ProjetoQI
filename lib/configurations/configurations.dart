c43174352dc86903794b12aeebbc5ddb6ae247e2
Pablo <pablo.carlottto@hotmail.com>
2024-07-18 20:55:21 -0300

    config

---
+++ lib/configurations/configurations.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_health_app/shared/custom_app_bar.dart';
+
+class Configurations extends StatefulWidget {
+  Configurations({super.key});
+
+  @override
+  State<Configurations> createState() => _ConfigurationsState();
+}
+
+class _ConfigurationsState extends State<Configurations> {
+  var switchRememberAccess = true;
+  var switch_two_factor_auth = false;
+  var switch_notifications = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar('Configurações'),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Switch(
+                    value: switch_notifications,
+                    onChanged: (isActive) {
+                      setState(() {
+                        switch_notifications = isActive;
+                      });
+                    }),
+                Text('Notificações')
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
